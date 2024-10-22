97f2b2c82f0251ac32ddfa93f7b7353db14c236a
Im-caddu <20241cas10010031@estudantes.ifpr.edu.br>
2024-10-21 22:19:33 -0300

    Utilizando vetores e loops

--- JavaScript/introJs.js
+++ JavaScript/introJs.js
@@ -1,27 +1,32 @@
-//Criar referências aos elementos que quero manipular
-
+// Criar referências aos elementos que quero manipular
 const frm = document.querySelector("form");
-const N1 = document.querySelector("#N1");
-const N2 = document.querySelector("#N2");
-const N3 = document.querySelector("#N3");
-const N4 = document.querySelector("#N4");
 const resp = document.querySelector("#mnome");
 const soma = document.querySelector("#soma");
 const media = document.querySelector("#media");
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault(); //evita o envio do formulário
+    e.preventDefault(); // evita o envio do formulário
+    
+    // Armazena os números em um array (vetor)
+    const numeros = [
+        Number(frm.inN1.value),
+        Number(frm.inN2.value),
+        Number(frm.inN3.value),
+        Number(frm.inN4.value)
+    ];
+    
+    // Somar os valores do vetor usando um loop
+    let total = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        total += numeros[i];
+    }
+    
+    // Calcular a média
+    const mediaValores = total / numeros.length;
+    
+    // Exibir o nome e os resultados
     const nome = frm.inNome.value;
-    console.log(nome);
-    const N1 = Number(frm.inN1.value);
-    console.log(N1);
-    const N2 = Number(frm.inN2.value);
-    console.log(N2);
-    const N3 = Number(frm.inN3.value);
-    console.log(N3);
-    const N4 = Number(frm.inN4.value);
-    console.log(N4);
     resp.innerText = `Olá ${nome}!`;
-    soma.innerText = `A soma dos números é ${N1 + N2 + N3 + N4}!`;
-    media.innerText = `A média dos números é ${(N1 + N2 + N3 + N4)/4}`;
+    soma.innerText = `A soma dos números é ${total}!`;
+    media.innerText = `A média dos números é ${mediaValores}`;
 });
